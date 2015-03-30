2c3949f4266b64db1296d411876c233a65dad953
nullifye <mohdrafhan@yahoo.com>
2015-03-30 23:21:54 +0800

    (pi:poslaju) change URL and terminal commands

--- tg_bot/plugins/poslaju.lua
+++ tg_bot/plugins/poslaju.lua
@@ -3,13 +3,16 @@ return function(msg)
   if args[1]==cmd then
     if #args == 2 then
       curr_ptt = os.time()
-      try = os.execute('wget -qO- --post-data \'ParcelNo='..args[2]..'\' http://www.pos.com.my/TrackAndTrace/viewDetailMelTrack/viewDetailMelTrack | sed -n \'/<table  class="QueryTableDetails">/,/<\\/table>/p\' | sed -e "s/^[ \t]*//" -e "s/&nbsp;//g" -e "/.*tr>/d" -e "/.*table>/d" -e "s/<[^>]*[>]//g" -e "s/^.$/----------/" > '..TMP_PATH..'/ppos'..curr_ptt..'.out')
+      try = os.execute('wget -qO- "http://api.pos.com.my/TrackNTraceWebApi/api/Details/'..args[2]..'" | grep -Po \'"date":.*?[^\\\\]",|"process":.*?[^\\\\]",|"office":.*?[^\\\\]",|},{\' | awk -F\'"[:]"\' \'{print $2}\' | sed -e "s/\\",//" | sed -e \':a;N;$!ba;s/\\n\\n/\\n----------\\n/g\' > '..TMP_PATH..'/ppos'..curr_ptt..'.out')
 
       if try then
+	    if filesize(TMP_PATH..'/ppos'..curr_ptt..'.out') == 0 then
+	      send_msg (target, "(pi:poslaju) record NOT FOUND", ok_cb, false)
+          return true
+	    end
+
 	    send_text (target, TMP_PATH.."/ppos"..curr_ptt..".out", ok_cb, false)
       end
-
-	  send_msg (target, "--end of request", ok_cb, false)
     else
       send_msg (target, "usage: pi:poslaju <TRACKING-NO>", ok_cb, false)
     end
