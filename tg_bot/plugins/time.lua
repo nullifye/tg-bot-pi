5a9a50cebb751ea901fd19fba4386ecad40aea5d
nullifye <mohdrafhan@yahoo.com>
2015-05-04 11:07:52 +0800

    (pi:time) new command

--- tg_bot/plugins/time.lua
+++ tg_bot/plugins/time.lua
@@ -12,7 +12,7 @@ return function(msg)
       searchq = string.gsub(searchq, "%s", "_")
 
       curr_time = os.time()
-      try = os.execute('wget -qO- http://www.thetimenow.com/clock/'..searchq..' --connect-timeout='..TIMEOUT..' | egrep "h1|main_time|main_date" | sed -e "s/<[^>]*[>]//g" -e "s/^[ \t]*//" -e "s/&nbsp;//g" > '..TMP_PATH..'/time'..curr_time..'.out')
+      try = os.execute('wget -qO- http://www.thetimenow.com/clock/'..searchq..'?embed=1 --connect-timeout='..TIMEOUT..' | egrep "span|main_time|main_date" | sed -e "s/<[^>]*[>]//g" -e "s/^[ \t]*//" -e "s/&nbsp;//g" | sed -e "1,2d" > '..TMP_PATH..'/time'..curr_time..'.out')
 
       if try then
         send_text (target, TMP_PATH.."/time"..curr_time..".out", ok_cb, false)
